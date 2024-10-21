96a0e31d916cddd29dee75d1680b0509d5b17cef
mohammadhhhhhh <9025722@student.zadkine.nl>
2024-10-21 15:23:54 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+let budget = 100;
+// let product = 60;
+let output = document.getElementById('output')
+let product = prompt();
+
+prompt()
+
+
+
+if(budget >=  product  ){
+console.log("ik heg genoeg geld")
+output.innerText = "ik heb genoeg geld"
+
+
+
+}else{
+console.log("te weinig geld")
+output.innerText = "ik heb te weinig geld"
+}
+
+
+
+console.log(budget);
